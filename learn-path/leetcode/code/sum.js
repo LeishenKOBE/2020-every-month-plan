09f9069752310eab2aba7cbb9c066ff356aa9068
LeishenKOBE <wuyujiesong@outlook.com>
2020-08-31 00:58:09 +0800

    fix: 重新写

--- learn-path/leetcode/code/sum.js
+++
@@ -1,5 +0,0 @@
-const sum = (a, b) => {
-  return Number(a) + Number(b)
-}
-
-console.log(sum(1, 2))
--- learn-path/leetcode/test/sum.test.js
+++
@@ -1,5 +0,0 @@
-import sum from '../code/sum'
-
-test('adds 1 + 2 equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
-})
