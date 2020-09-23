c3a6490a15e37030b226251b79b9fbd8946c08a3
shidongzhao <wuyujiesong@gmailcom>
2020-09-23 21:57:15 +0800

    fix: 修改

--- learn-path/vue3.0/todolist/src/App.vue
+++ learn-path/vue3.0/todolist/src/App.vue
@@ -1,11 +1,24 @@
 <template>
-  <div></div>
+  <div>{{count}}:{{double}}</div>
+  <button @click="increment">+1</button>
 </template>
 
 <script>
+import { ref, computed } from "vue";
+
 export default {
-  name: "App",
-  components: {},
+  setup() {
+    let count = ref(0);
+    let double = computed(() => count.value * 2);
+    function increment() {
+      count.value++;
+    }
+    return {
+      count,
+      double,
+      increment,
+    };
+  },
 };
 </script>
 
