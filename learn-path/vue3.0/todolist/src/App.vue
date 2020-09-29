1d2cd6f0bb18a670dac38ad1bc1d5773eaf018fc
shidongzhao <wuyujiesong@gmailcom>
2020-09-29 22:40:03 +0800

    学习vue3.0

--- learn-path/vue3.0/todolist/src/App.vue
+++ learn-path/vue3.0/todolist/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>{{ count }}:{{ double }}</div>
   <button @click="increment">+1</button>
+  <button @click="getName">getName</button>
+  <div ref="div">sdaasd</div>
 </template>
 
 <script>
@@ -8,7 +10,8 @@ import { ref, computed, onMounted } from "vue";
 import TalTools from "@tal/common-tool-function";
 
 export default {
-  setup() {
+  setup(props, ctx) {
+    console.log(props, ctx);
     let count = ref(0);
     let double = computed(() => count.value * 2);
 
@@ -21,10 +24,18 @@ export default {
     function increment() {
       count.value++;
     }
+
+    const div = ref(null);
+
+    const getName = () => {
+      console.log(div.value);
+    };
     return {
       count,
       double,
       increment,
+      div,
+      getName,
     };
   },
 };
