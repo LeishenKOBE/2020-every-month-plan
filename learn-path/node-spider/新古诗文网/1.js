9d5da4530562080490541c392fb5ec0375eb63f7
LeishenKOBE <wuyujiesong@outlook.com>
2020-11-30 23:17:29 +0800

    古诗词网

--- learn-path/node-spider/新古诗文网/1.js
+++ learn-path/node-spider/新古诗文网/1.js
@@ -1,14 +1,15 @@
-const request = require('request');
-const cheerio = require('cheerio');
+const request = require("request");
+const cheerio = require("cheerio");
 
-// for(let i=1;i<2;i++){
-
-// }
-
-let url = 'https://www.gushiwen.org/';
-request({
+let url = "https://www.gushiwen.org/";
+request(
+  {
     url,
-    method: 'get'
-}, (err,res,body) => {
-    console.log(body)
-})
+    method: "get",
+  },
+  (err, res, body) => {
+    const $ = cheerio.load(body);
+    const left = $(".main3 .left .sons .cont").text();
+    console.log(left);
+  }
+);
