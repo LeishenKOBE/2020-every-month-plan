67e59cdc8060a0a8fadbd39f8ce9b89640169b7a
LeishenKOBE <wuyujiesong@outlook.com>
2020-12-04 15:04:17 +0800

    feat: 新增webpack插件

---
+++ learn-path/webpackLoaderAndPlugin/first/src/loaders/md2html.js
@@ -0,0 +1,13 @@
+import { getOptions } from "loader-utils";
+import validateOptions from "schema-utils";
+
+const schema = {
+  type: "object",
+  properties: {
+    test: {
+      type: "string",
+    }
+  },
+};
+
+module.exports = function () {};
---
+++ learn-path/webpackLoaderAndPlugin/first/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
