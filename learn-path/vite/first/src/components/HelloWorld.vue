59720c56e4a58ad66a4d8b96b3c17b545e3c5037
大家好我叫石同学 <wuyujiesong@outlook.com>
2020-12-26 20:24:48 +0800

    vite学习

---
+++ learn-path/vite/first/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-for="(item, key) in state" :key="key">{{ item.name }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+interface Course {
+  id: number;
+  name: string;
+  phone: string;
+  address: any;
+  company: any;
+  email: string;
+  username: string;
+  website: string;
+}
+
+export default defineComponent({
+  setup() {
+    let state = ref<Course[]>([]);
+    fetch("/api/users")
+      .then(response => response.json())
+      .then(json => {
+        state.value.push(...json);
+      });
+
+    return {
+      state,
+    };
+  },
+});
+</script>
