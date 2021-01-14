66526372c9bb5d747f9be89ac02543c662ea9786
大家好我叫石同学 <wuyujiesong@outlook.com>
2021-01-15 01:52:00 +0800

    Update 1.js

--- learn-path/跳槽专用文件夹/算法/1.js
+++ learn-path/跳槽专用文件夹/算法/1.js
@@ -3,9 +3,26 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function (prices) {
-  let profit = 0;
-  for (let i = 0; i < prices.length; i += 1) {
-    let temp = prices[i] - prices[i - 1];
-  }
+// var maxProfit = function (prices) {
+//   let profit = 0;
+//   for (let i = 0; i < prices.length; i += 1) {
+//     let temp = prices[i] - prices[i - 1];
+//   }
+// };
+
+var minPathSum = function (grid) {
+	let m = grid.length;
+	let n = grid[0].length;
+	let dp = [];
+	for (let i = 0; i < m; i += 1) {
+		for (let j = 0; j < n; j += 1) {
+			dp[i][j] = 0;
+		}
+	}
+	console.log(dp);
 };
+minPathSum([
+	[1, 3, 1],
+	[1, 5, 1],
+	[4, 2, 1],
+]);
